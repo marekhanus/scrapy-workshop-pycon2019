c7888b3561b4cb7fbfd626e555fc375917eb7ea8
Marek Hanuš <marekdiamo@gmail.com>
2019-06-16 16:30:25 +0200

    Implement quotation spider

--- projects/workshop/workshop/spiders/quotation.py
+++ projects/workshop/workshop/spiders/quotation.py
@@ -8,4 +8,13 @@ class QuotationSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        pass
+        for quote in response.xpath('//div[@class="quote"]'):
+            yield {
+                'text': quote.xpath('./span[@class="text"]/text()').extract_first(),
+                'author': quote.xpath('.//small[@class="author"]/text()').extract_first(),
+                'tags': quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').extract(),
+            }
+
+        next_url_link = response.xpath('//nav//li[@class="next"]/a/@href').extract_first()
+        if next_url_link is not None:
+            yield scrapy.Request(response.urljoin(next_url_link))
