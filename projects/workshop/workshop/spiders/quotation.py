b9546fc294064a7556ef2a8c0917b7856bad1fdf
Marek Hanuš <marekdiamo@gmail.com>
2019-06-16 16:37:10 +0200

    Testing quotation pipeline

--- projects/workshop/workshop/spiders/quotation.py
+++ projects/workshop/workshop/spiders/quotation.py
@@ -10,6 +10,7 @@ class QuotationSpider(scrapy.Spider):
     def parse(self, response):
         for quote in response.xpath('//div[@class="quote"]'):
             yield {
+                # 'text': None,  # just for testing quotation pipeline
                 'text': quote.xpath('./span[@class="text"]/text()').extract_first(),
                 'author': quote.xpath('.//small[@class="author"]/text()').extract_first(),
                 'tags': quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').extract(),
